7eab0aa2c21b53aae715b0d80247514ba9c5aae3
MilanVD <99069122@mydavinci.nl>
2021-10-07 10:00:08 +0200

    aftellen.py

---
+++ Tafelmanieren/aftellen.py
@@ -0,0 +1,7 @@
+#aftellen
+import time
+for i in range(30,-1,-1):  
+    time.sleep(1)
+    print(i)
+
+print("De lancering is succesvol!")
---
+++ Tafelmanieren/evengetallen.py
@@ -0,0 +1,6 @@
+#evengetallen
+import time
+for i in range(20, 51,2):
+    time.sleep(1)
+    print(i)
+print("hier heb je ze :)")
